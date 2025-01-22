cfc442af3d0db3d28400a092dd72f15f45040468
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-01-22 23:12:19 +0530

    created the collection page and deleted blogs page.
    - created the collection page using single child scroll view.

--- insta_pro/lib/collection/collection.dart
+++ insta_pro/lib/collection/collection.dart
@@ -5,6 +5,9 @@ class Collection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const SingleChildScrollView(),
+    );
   }
 }

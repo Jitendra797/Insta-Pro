f97e2728451a766fbaffe3547140ee263755dc5a
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-01-29 23:45:20 +0530

    inserts the blogs cards in the deleted blogs
    - inserts the blogs cards in the deleted blogs

--- insta_pro/lib/deletedblogs/deletedblogs.dart
+++ insta_pro/lib/deletedblogs/deletedblogs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:insta_pro/blogcard/blogcard.dart';
 
 class Deletedblogs extends StatelessWidget {
   const Deletedblogs({super.key});
@@ -7,7 +8,17 @@ class Deletedblogs extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const SingleChildScrollView(),
+      body: const SingleChildScrollView(
+          child: Column(
+        children: [
+          BlogCard(),
+          BlogCard(),
+          BlogCard(),
+          BlogCard(),
+          BlogCard(),
+          BlogCard()
+        ],
+      )),
     );
   }
 }
